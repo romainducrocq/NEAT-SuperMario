648577019ac6d0536d949eaeceb592632e2d8d32
romainducrocq <romain.g.ducrocq@gmail.com>
2022-11-22 17:17:33 +0100

    rom

--- bin/nintaco.sh
+++ bin/nintaco.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-java/jre1.8.0_latest/bin/java -jar nintaco/Nintaco_bin_2020-05-01/Nintaco.jar &
+ROM="Super Mario Bros (E).nes"
+
+java/jre1.8.0_latest/bin/java -jar nintaco/Nintaco_bin_2020-05-01/Nintaco.jar '../res/rom/'"${ROM}" &
