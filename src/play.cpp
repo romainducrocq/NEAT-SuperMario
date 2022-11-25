29b4d305eca1938194633bbcb591b329ef2abf06
romain <romainducrocq>
2022-11-25 17:37:15 +0100

    neat

--- src/play.cpp
+++ src/play.cpp
@@ -53,8 +53,6 @@ void App::Play::init()
     }
 
     this->env.epoch = 0;
-
-    this->reset();
 }
 
 void App::Play::reset()
@@ -73,6 +71,11 @@ void App::Play::reset()
 
 void App::Play::step()
 {
+    if(this->env.ini){
+        this->reset();
+        this->env.ini = false;
+    }
+
     this->env.step_func();
 
     this->env.step_render_func();
